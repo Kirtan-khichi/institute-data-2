bc83073bda3e9d4161463de9ce20a1a2d639316d
kirtan <115882237+Kirtan-khichi@users.noreply.github.com>
2022-10-15 21:17:11 +0530

    Update my first program.py

--- my first program.py
+++ my first program.py
@@ -1,5 +1,4 @@
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
+
 print("Hello Dear Student Form For STARE Scholarship Program Enrollment ")
 def repeat():
 	institute=input('enter your institute name :-')
